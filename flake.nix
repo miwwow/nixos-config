a1487ce7eabdd356b1d87b2536efcbda584b831f
miwwow <admin@mirror.gay>
2025-12-05 11:10:30 +0000

    Simplify flake

--- flake.nix
+++ flake.nix
@@ -13,18 +13,24 @@
       spicetify-nix,
       ...
     }@inputs:
+    let
+      inherit (self) outputs;
+
+      module_paths = [
+        inputs.milk-grub-theme.nixosModule
+        spicetify-nix.nixosModules.spicetify
+        ./configuration.nix
+        ./apps
+      ];
+    in
     {
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            inputs.milk-grub-theme.nixosModule
-            spicetify-nix.nixosModules.spicetify
-
-            ./configuration.nix
-            ./apps
-          ];
+          specialArgs = { 
+	    inherit inputs outputs;
+	  };
+          modules = module_paths;
         };
       };
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-tree;
