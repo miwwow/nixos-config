48922cb6ef35cd11986ddde7d94939a190e64bd7
miwwow <johndoe@nixos.com>
2026-08-09 14:59:17 -0400

    Enable overlays

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -28,9 +28,9 @@
     # You can add overlays here
     overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
-      # inputs.self.overlays.additions
-      # inputs.self.overlays.modifications
-      # inputs.self.overlays.unstable-packages
+      inputs.self.overlays.additions
+      inputs.self.overlays.modifications
+      inputs.self.overlays.unstable-packages
 
       # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
